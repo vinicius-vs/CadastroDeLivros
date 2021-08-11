e79d9b4349a3259e0ec3cf7493c449159d91c077
vinicius <vinicius_stumpf@edu.univali.br>
2021-08-11 10:23:19 -0300

    Criando o base repository generico

---
+++ LivroLibrary/LivroLibrary/Models/LivroBibliotecaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivroLibrary.Models
+{
+    class LivroBibliotecaModel: LivroBaseModel
+    {
+        public string Localizacao { get; set; }
+
+        public bool Disponibilidade { get; set; }
+
+    }
+}
